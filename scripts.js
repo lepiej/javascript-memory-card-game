98016bc963bfacd15a8da834bfcbf72a2a140c78
Beata Krzemińska <122975499+lepiej@users.noreply.github.com>
2023-01-28 14:39:28 +0100

    script-breakdown
    These changes were made to add comments and documentation to explain the purpose and function of the code. This was done to help me better understand the code and looking back to track my progress. In the spirit of the principle of "if you can't explain it, you don't understand it".

--- scripts.js
+++ scripts.js
@@ -13,7 +13,8 @@ var cards = [
   { value: "7", image: "./images/clubs_7.svg", matched: false },
 ];
 
-var cardEls = document.querySelectorAll(".card");
+var cardEventlistener = document.querySelectorAll(".card");
+// store the index of the first guess
 var firstGuess = null;
 var canGuess = true;
 var flippedCards = 0;
@@ -40,20 +41,30 @@ function shuffle(array) {
 
 shuffle(cards);
 
-cardEls.forEach(function (element, index) {
+cardEventlistener.forEach(function (element, index) {
   element.addEventListener("click", function () {
     // debug
-    if (index === firstGuess || cards[index].matched || !canGuess) {
+    if (
+      // if the current element that was clicked is the same as the first guess
+      index === firstGuess ||
+      // if the card has already been matched
+      cards[index].matched ||
+      // if the player is not allowed to make a guess at the moment
+      !canGuess
+    ) {
       alert("invalid guess");
       return;
     }
 
     var clickedCard = cards[index];
+    // show the flipped image of the card (front)
     element.setAttribute("src", clickedCard.image);
 
     if (firstGuess === null) {
+      // add value to the first guess
       firstGuess = index;
-    } else {
+    } // second guess
+    else {
       guesses++;
       document.querySelector("#guesses").textContent = guesses;
       // match
@@ -71,8 +82,13 @@ cardEls.forEach(function (element, index) {
         // no match
         canGuess = false;
         setTimeout(function () {
-          cardEls[firstGuess].setAttribute("src", "./images/red2.svg");
-          cardEls[index].setAttribute("src", "./images/red2.svg");
+          cardEventlistener[firstGuess].setAttribute(
+            "src",
+            "./images/red2.svg"
+          );
+          // flip back both over (front -> back)
+          cardEventlistener[index].setAttribute("src", "./images/red2.svg");
+          //starts a new turn
           firstGuess = null;
           canGuess = true;
         }, 500);
@@ -85,19 +101,19 @@ function resetGame() {
   // delay after a win
   canGuess = false;
   setTimeout(function () {
-    // changing back to the initial values
+    // change back to the initial values
     firstGuess = null;
     canGuess = true;
     flippedCards = 0;
     guesses = 0;
     document.querySelector("#guesses").textContent = guesses;
 
-    // flipping upside down
-    cardEls.forEach(function (element, index) {
+    // flipp upside down
+    cardEventlistener.forEach(function (element, index) {
       element.setAttribute("src", "./images/red2.svg");
     });
 
-    // reseting matched positions
+    // reset matched positions
     cards.forEach(function (card, index) {
       card.matched = false;
     });
